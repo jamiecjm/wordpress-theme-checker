11d0e3f826ea834578acdfefdd7cde5a94cd48d0
jiemeechong <jamiecjm@gmail.com>
2018-05-01 15:18:53 +0800

    Add home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :theme_versions
   resources :themes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'pages#home'
 end
